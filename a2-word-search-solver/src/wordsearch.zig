afeab4ef890aaba26651ba7a5321bb338d79482f
Jacob Bahn <jacobabahn@gmail.com>
2024-09-10 21:47:40 -0700

    Inefficient but finished the solver

--- a2-word-search-solver/src/wordsearch.zig
+++ a2-word-search-solver/src/wordsearch.zig
@@ -7,6 +7,7 @@ pub fn solve(matrix: lm.LetterMatrix, wordlist: std.ArrayList([]const u8)) lm.Le
         for (matrix, 0..) |row, i| {
             for (row, 0..) |_, j| {
                 if (matrix[i][j] == word[0]) {
+                    // std.debug.print("Letter: {c}, word: {s}, r {d}, c: {d}\n", .{ matrix[i][j], word, i, j });
                     const found = checkDirections(matrix, word, i, j);
                     if (found) {
                         count += 1;
@@ -37,16 +38,23 @@ fn checkDirections(matrix: lm.LetterMatrix, word: []const u8, row: usize, col: u
     for (directions) |direction| {
         const irow: isize = @intCast(row);
         const icol: isize = @intCast(col);
+        if (irow + direction[0] < 0 or icol + direction[1] < 0) {
+            continue;
+        }
         const nr: usize = @intCast(irow + direction[0]);
         const nc: usize = @intCast(icol + direction[1]);
         const wordLen: isize = @intCast(word.len);
         var found = false;
 
         if (!canPlaceWord(row, col, wordLen, matrix.len, &direction)) {
-            return false;
+            continue;
         }
         if (matrix[nr][nc] == word[1]) {
+            // std.debug.print("Found 2nd letter of word: {s} at r: {d}, c: {d}\n", .{ word, nr, nc });
             found = checkWord(matrix, word, row, col, &direction);
+            if (found) {
+                std.debug.print("Found word: '{s}' at r: {d}, c: {d}\n", .{ word, nr, nc });
+            }
         }
 
         if (found) {
@@ -61,16 +69,18 @@ fn canPlaceWord(r: usize, c: usize, wordLen: isize, size: usize, direction: []co
     const nc: isize = @intCast(c);
     const row = nr + @as(isize, direction[0]) * @as(isize, wordLen);
     const col = nc + @as(isize, direction[1]) * @as(isize, wordLen);
-    std.debug.print("canPlaceWord: row: {d}, col: {d}\n, direction: {any}\n", .{ row, col, direction });
+    // std.debug.print("canPlaceWord: row: {d}, col: {d}\n, direction: {any}\n", .{ row, col, direction });
+    // std.debug.print("Return val of CanPlaceWord: {any}\n", .{row >= 0 and row < size and col >= 0 and col < size});
     return row >= 0 and row < size and col >= 0 and col < size;
 }
 
 fn checkWord(matrix: lm.LetterMatrix, word: []const u8, row: usize, col: usize, direction: *const [2]isize) bool {
     // const wordLen: isize = @intCast(word.len);
+    // std.debug.print("In checkword: '{s}' at row: {d}, col: {d} with direction: {any}\n", .{ word, row, col, direction });
 
     const nrow: isize = @intCast(row);
     const ncol: isize = @intCast(col);
-    for (word, 1..) |letter, i| {
+    for (word, 0..) |letter, i| {
         const idx: usize = i;
         const newRow: usize = @intCast(nrow + direction[0] * @as(isize, @intCast(idx)));
         const newCol: usize = @intCast(ncol + direction[1] * @as(isize, @intCast(idx)));
